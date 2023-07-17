9db5186f28ce523146b89f23c193ca03eabdca84
ravisharma <rssharmakapro@gmail.com>
2023-07-17 02:28:20 -0700

    updates

--- src/Pages/Template1/Home1.jsx
+++ src/Pages/Template1/Home1.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar1 from './Navbar1'
-import { Box, Flex,Image,Button,Text } from '@chakra-ui/react'
+import { Box, Flex,Image,Button,Text,Heading } from '@chakra-ui/react'
 import '../../CSS/Home1.css'
 import Footer1 from './Footer1'
 
@@ -25,6 +25,29 @@ return (
         <Box className='brandBox' borderRadius={20} h={200} mt={-90} w={180} background='white' border='2px solid black'><Image h={200} borderRadius={20} src='https://www.manutan.com/blog/medias/files/thumbsSource/CFO_1110x555.jpg'/>
         <Text>CFO</Text></Box>
     </Flex>
+
+
+    <Box mt='170px'>
+        <Image src='https://dex-bin.bnbstatic.com/new/static/images/home/banner-bottom-bg.png'/>
+        <Flex w='80%' m='auto'gap={20}> 
+            <Box>
+                <Heading pb={5}>About us</Heading>
+                <Text textAlign='left'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, nobis corrupti minima necessitatibus culpa quaerat doloribus libero eveniet nam odio laboriosam in aut, consequuntur assumenda ipsam officiis adipisci voluptatibus totam. Expedita, odio!
+                    <br />
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, blanditiis ratione! Nostrum qui molestiae quod adipisci et a asperiores atque?
+                    <br />
+                    <br />
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, blanditiis ratione! Nostrum qui molestiae quod adipisci et a asperiores atque? <br />
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, blanditiis ratione! Nostrum qui molestiae quod adipisci et a asperiores atque?
+                </Text>
+            </Box>
+            <Image borderRadius={20} src='https://admin.azbigmedia.com/wp-content/uploads/2018/11/real-estate-agent.jpg'/>
+        </Flex>
+    </Box>
+
+
+
+
     <Flex justifyContent='space-around'  pt={50} w='60%' m='auto' gap={40}>
     <Box w='50%'>
         <Image src='https://images.squarespace-cdn.com/content/v1/59b892162278e7ce6ec4c43c/1681326962477-S0N2M9H0WBBC14MHZEA8/Final+MS+Sticker.png?format=2500w'/>

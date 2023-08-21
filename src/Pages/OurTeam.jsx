dda670ad61ce0c5be95d771ad8217eac4ec25b9a
Ravi sharma <rsharma80595@gmail.com>
2023-08-21 17:51:31 +0530

    new cahagnes

--- src/Pages/OurTeam.jsx
+++ src/Pages/OurTeam.jsx
@@ -8,7 +8,12 @@ import BOD from './BOD'
 import Popup from '../Components/Popup'
 import cover from '../Images/cover.png'
 import BOD3 from '../Images/BOD3.png'
-
+import Team1 from '../Images/Team1.png'
+import Team2 from '../Images/Team2.png'
+import Team3 from '../Images/Team3.png'
+import Team4 from '../Images/Team4.png'
+import Team5 from '../Images/Team5.png'
+import Team6 from '../Images/Team6.png'
 
 
 const OurTeam = () => {
@@ -29,20 +34,18 @@ return(
 </Box>
         <Box w={"95%"} m='auto'>
         <Flex direction={['column','column','column','row']} gap={10} justifyContent='space-around' w={'80%'} m={'auto'}>
-        <Box backgroundColor='white' textAlign='center'>
-          <Image w={'650px'} h={['250px','200px','200px','250px']} src={BOD2}/>
+        <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center'>
+          <Image borderTopRadius={10} w={'650px'} h={['250px','200px','200px','250px']} src={BOD2}/>
             <Text pt={2}>Shreyansh Verma</Text>
-            {/* <Text fontWeight={'bold'}>Founder</Text> */}
           </Box>
-          <Box backgroundColor='white' textAlign='center'>
-            <Image w={'650px'} h={['250px','200px','200px','250px']} src={BOD1}/>
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} backgroundColor='white' textAlign='center' borderRadius={10}>
+            <Image borderTopRadius={10} w={'650px'} h={['250px','200px','200px','250px']} src={BOD1}/>
             <Text pt={2}>Mohit Jain</Text>
             <Text fontWeight={'bold'}>Founder</Text>
           </Box>
-          <Box backgroundColor='white' textAlign='center' >
-          <Image w={'550px'} h={['250px','200px','200px','250px']} src={BOD3}/>
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center' >
+          <Image borderTopRadius={10} w={'650px'} h={['250px','200px','200px','250px']} src={BOD3}/>
             <Text pt={2}>Amitesh  Mathur </Text>
-            {/* <Text fontWeight={'bold'}>Founder</Text> */}
           </Box>
           
         </Flex>
@@ -50,43 +53,60 @@ return(
       </Box>
     </Box>
 
-    <Box background={'#efefef'} pb={150}>
+    <Box background={'#efefef'} pb={100}>
     <Box backgroundColor='#efefef' pb={10}>
         <Box w={"95%"} m='auto'>
-        {/* <Text pb={10} fontSize={['26px','26px','36px','36px']} fontFamily={'Prata'}>Our Board</Text> */}
         <Flex direction={['column','column','column','row']} gap={10} justifyContent='space-around' w={'80%'} m={'auto'}>
-          <Box backgroundColor='white' textAlign='center'>
-            <Image w={'650px'} h={['250px','200px','200px','200px']} src={BOD1}/>
-            <Text pt={2}>Mohit Jain</Text>
-            <Text fontWeight={'bold'}>Founder</Text>
-            {/* <Button onClick={openPopupWithData1} mt={5} mb={3} style={{backgroundColor:'rgb(48, 52, 65)',color:'white'}} textAlign={'center'}>Know More</Button> */}
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center'>
+            <Image borderTopRadius={10} w={'650px'} h={['250px','200px','200px','200px']} src={Team1}/>
+            <Text pt={2}>Sonali Sharma</Text>
+            {/* <Text fontWeight={'bold'}>Founder</Text> */}
           </Box>
-          <Box backgroundColor='white' textAlign='center'>
-          <Image w={'650px'} h={['250px','200px','200px','200px']} src={BOD2}/>
-            <Text>Rich Wacksman</Text>
-            <Text fontWeight={'bold'}>Founder</Text>
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center'>
+          <Image borderTopRadius={10} w={'650px'} h={['250px','200px','200px','200px']} src={Team2}/>
+            <Text pt={2}>Sunit kochhar</Text>
+            {/* <Text fontWeight={'bold'}>Founder</Text> */}
            
-            {/* <Button onClick={openPopupWithData2} mt={5} mb={3} style={{backgroundColor:'rgb(48, 52, 65)',color:'white'}} textAlign={'center'}>Know More</Button> */}
           </Box>
-          <Box backgroundColor='white' textAlign='center'>
-          <Image w={'650px'} h={['250px','200px','200px','200px']} src={BOD2}/>
-            <Text>Rich Wacksman</Text>
-            <Text fontWeight={'bold'}>Founder</Text>
-    
-            {/* <Button mt={5} onClick={openPopupWithData3} mb={3} style={{backgroundColor:'rgb(48, 52, 65)',color:'white'}} textAlign={'center'}>Know more</Button> */}
-          </Box>
-          <Box backgroundColor='white' textAlign='center'>
-          <Image w={'650px'} h={['250px','200px','200px','200px']} src={BOD1}/>
-            <Text>Rich Wacksman</Text>
-            <Text fontWeight={'bold'}>Founder</Text>
-            {/* <Button  onClick={openPopupWithData4} mt={5} mb={3} style={{backgroundColor:'rgb(48, 52, 65)',color:'white'}} textAlign={'center'}>Know more</Button> */}
-            {/* <Popup isOpen={isOpen} onClose={closePopup} dynamicData={dynamicData} /> */}
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center'>
+          <Image borderTopRadius={10} w={'650px'} h={['250px','200px','200px','200px']} src={Team3}/>
+            <Text pt={2}>Rashmi Singh</Text>
+            {/* <Text fontWeight={'bold'}>Founder</Text> */}
+            </Box>
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center'>
+          <Image borderTopRadius={10} w={'650px'} h={['250px','200px','200px','200px']} src={Team6}/>
+            <Text pt={2}>Nikhil Nigam</Text>
+            {/* <Text fontWeight={'bold'}>Founder</Text> */}
           </Box>
         </Flex>
         </Box>
       </Box>
     </Box>
       
+
+
+    <Box background={'#efefef'} pb={150}>
+    <Box backgroundColor='#efefef' pb={10}>
+        <Box w={"95%"} m='auto'>
+        <Flex direction={['column','column','column','row']} justifyContent={'center'}  w={'80%'} m={'auto'} gap={'50px'}>
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center'>
+          <Image borderTopRadius={10} w={'250px'} h={['250px','200px','200px','200px']} src={Team4}/>
+            <Text pt={2}>Kishan Thakur</Text>
+            {/* <Text fontWeight={'bold'}>Founder</Text> */}
+          </Box>
+          <Box _hover={{ transform: 'translateY(-15px)', transition: 'transform 0.5s' }} borderRadius={10} backgroundColor='white' textAlign='center'>
+          <Image borderTopRadius={10} w={'250px'} h={['250px','200px','200px','200px']} src={Team5}/>
+            <Text pt={2}>Anil Prsad</Text>
+            {/* <Text fontWeight={'bold'}>Founder</Text> */}
+          </Box>
+        </Flex>
+        </Box>
+      </Box>
+    </Box>
+
+
+
+
     <Footer/>
     </Box> 
   )

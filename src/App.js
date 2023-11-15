f144ec9815e23ee534a2ad57a62c4d09bd348f9c
Ravi sharma <rsharma80595@gmail.com>
2023-11-15 14:50:03 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import AllRoute from './AllRoutes/AllRoute';
 import './App.css';
-
+import ReactGA from 'react-ga';
 
 function App() {
+  ReactGA.initialize('G-TWK62TEZM1');
 
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
 
 return (
